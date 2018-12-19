424897d513f645bf5236784cb3b2fc27a1267850
碰碰 <pmashiro@mail.piwi.com.tw>
2018-12-19 18:06:03 +0800

    add font plugin

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -27,6 +27,7 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import Font from '@ckeditor/ckeditor5-font/src/font';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -52,7 +53,8 @@ ClassicEditor.builtinPlugins = [
 	Paragraph,
 	PasteFromOffice,
 	Table,
-	TableToolbar
+	TableToolbar,
+	Font
 ];
 
 // Editor configuration.
@@ -63,6 +65,7 @@ ClassicEditor.defaultConfig = {
 			'|',
 			'bold',
 			'italic',
+			'fontSize',
 			'link',
 			'bulletedList',
 			'numberedList',
@@ -89,6 +92,9 @@ ClassicEditor.defaultConfig = {
 			'mergeTableCells'
 		]
 	},
+	fontSize: {
+		options: [9, 11, 13, 'default', 17, 19, 21]
+	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en'
+	language: 'zh'
 };
